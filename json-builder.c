4b75d330b94d8b2fcf18f6ff731c2020c62d4af0
James McLaughlin <james@clickteam.com>
2014-08-15 18:54:32 +0100

    Add snprintf -> _snprintf macro for MSVC

--- json-builder.c
+++ json-builder.c
@@ -34,6 +34,10 @@
 #include <assert.h>
 #include <stdio.h>
 
+#ifdef _MSC_VER
+    #define snprintf _snprintf
+#endif
+
 const static json_serialize_opts default_opts =
 {
    json_serialize_mode_single_line,
