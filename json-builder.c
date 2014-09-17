11f25f6a22010597fe8c1780924a397f97120a5e
Josh DeWitt <Josh.DeWitt@garmin.com>
2014-09-17 07:46:38 -0500

    Missed the compiler issue in the measure_ex function.
    Change-Id: I5ea71b9685b5a67b62d7391e83eab9c25fe62bab
    Type: Bug Fix

--- json-builder.c
+++ json-builder.c
@@ -593,7 +593,8 @@ size_t json_measure_ex (json_value * value, json_serialize_opts opts)
                MEASURE_NEWLINE();
             }
 
-            value = value->u.array.values [((json_builder_value *) value)->length_iterated ++];
+            ((json_builder_value *) value)->length_iterated++;
+            value = value->u.array.values [((json_builder_value *) value)->length_iterated - 1];
             continue;
 
          case json_object:
